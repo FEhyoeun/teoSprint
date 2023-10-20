f6d5ce37698ecb70cdaab2aed45a284d822633ac
FEhyoeun <khyoeun92@gmail.com>
2023-10-21 00:23:39 +0900

    chore: 초기 셋팅

---
+++ teo_sprint2/.eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended', // 새로 추가
+    'plugin:jsx-a11y/recommended', // 새로 추가
+    'plugin:prettier/recommended', // 새로 추가
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'], // 새로 추가
+}
