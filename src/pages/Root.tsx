de3a8be86564f95e7ba878b1b6aa617dba03fa8b
UHquokka <bananamayong@gmail.com>
2023-10-21 19:00:48 +0900

    Root 페이지 내부 수정

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,10 +1,11 @@
+import { Outlet } from 'react-router-dom';
 import { Layout } from '../components/Layout';
 
 export const Root = () => {
   return (
     <>
       <h1>Root</h1>
-      <Layout />
+      <Outlet />
     </>
   );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,26 +1,28 @@
-import { createBrowserRouter } from 'react-router-dom';
-import { Root } from '../pages/Root';
-import { Layout } from '../components/Layout';
-import { Category } from '../pages/Category';
-import { Profile } from '../pages/Profile';
+import {
+  createBrowserRouter,
+} from "react-router-dom";
+import { Root } from "../pages/Root";
+import { Layout } from "../components/Layout";
+import { Category } from "../pages/Category";
+import { Profile } from "../pages/Profile";
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Root />,
     children: [
       {
-        path: '',
+        path: "",
         element: <Layout />,
         children: [
           {
-            path: 'category',
+            path: "category",
             element: <Category />,
           },
-        ],
+        ]
       },
       {
-        path: 'profile',
+        path: "profile",
         element: <Profile />,
       },
     ],
