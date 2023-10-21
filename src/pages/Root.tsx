fc11a39ca93e563d75fea3c97733cfbdf0090895
SongInJae <pan0663@naver.com>
2023-10-21 21:41:52 +0900

    feat: 프로필 페이지 구현

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,11 +1,37 @@
-import { Outlet } from 'react-router-dom';
-import { Layout } from '../components/Layout';
+import React, { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Wrapper = styled.div`
+  width: 100vw;
+  max-height: 700px;
+  height: 100vh;
+  border: 1px solid black;
+`;
 
 export const Root = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const storedName = localStorage.getItem('name');
+    if (storedName) {
+      navigate('/category');
+    } else {
+      navigate('/profile');
+    }
+  }, [navigate]);
+
   return (
-    <>
-      <h1>Root</h1>
-      <Outlet />
-    </>
+    <Container>
+      <Wrapper>
+        <Outlet />
+      </Wrapper>
+    </Container>
   );
 };
