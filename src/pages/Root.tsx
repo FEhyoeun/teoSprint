9da421505a7ad2d85f0e6f8ff3fdfc6355e33d58
UHquokka <bananamayong@gmail.com>
2023-10-21 22:15:19 +0900

    RootPageCss 유지

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,5 +1,21 @@
-import { Outlet } from 'react-router-dom';
-import { Layout } from '../components/Layout';
+import React, { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Wrapper = styled.div`
+  width: 100vw;
+  max-height: 700px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export const Root = () => {
   const navigate = useNavigate();
@@ -12,7 +28,6 @@ export const Root = () => {
       navigate('/profile');
     }
   }, [navigate]);
-
   return (
     <Container>
       <Wrapper>
