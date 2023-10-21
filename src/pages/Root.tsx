7313d3cc71b7821cf3727db4051d3e31442af953
UHquokka <bananamayong@gmail.com>
2023-10-21 21:59:25 +0900

    style: root page css 수정 및 임시 변수 지정

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,11 +1,28 @@
+import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { Layout } from '../components/Layout';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Wrapper = styled.div`
+  width: 100vw;
+  max-height: 700px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export const Root = () => {
   return (
-    <>
-      <h1>Root</h1>
-      <Outlet />
-    </>
+    <Container>
+      <Wrapper>
+        <Outlet />
+      </Wrapper>
+    </Container>
   );
 };
