c65d2d3ba0499e2a083fbd8c1d4efa480f8ee2ce
FEhyoeun <khyoeun92@gmail.com>
2023-10-21 12:59:53 +0900

    feat: router 기본 처리 추가

---
+++ teo_sprint2/src/routes/index.tsx
@@ -0,0 +1,30 @@
+import {
+    createBrowserRouter,
+} from "react-router-dom";
+import { Root } from "../pages/Root";
+import { Layout } from "../components/Layout";
+import { Category } from "../pages/Category";
+import { Profile } from "../pages/Profile";
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          path: "",
+          element: <Layout />,
+          children: [
+            {
+                path: "category",
+                element: <Category />,
+              },
+          ]
+        },
+        {
+            path: "profile",
+            element: <Profile />,
+        },
+      ],
+    },
+  ]);
