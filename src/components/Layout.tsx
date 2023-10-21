a8514033bd83d98e9ab22b86a0a852f5cfda4267
UHquokka <bananamayong@gmail.com>
2023-10-21 20:56:46 +0900

    layoutPage: 준비중인 play 버튼 추가

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,13 +3,19 @@ import SideNavBar from './navBars/SideNavBar';
 import TopNavBar from './navBars/TopNavBar';
 
 export const Layout = () => {
+  const onPlayBtnClick = () => {
+    alert('준비중인 서비스 입니다');
+  };
   return (
     <>
       <h2>Layout</h2>
       {/* 마스코트 생성시 대체 */}
       <Link to="/">Home</Link>
       <TopNavBar />
-      <SideNavBar />
+      <div>
+        <button onClick={onPlayBtnClick}>▶️</button>
+        <SideNavBar />
+      </div>
       <Outlet />
     </>
   );
