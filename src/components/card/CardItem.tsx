f0b4e15b11e0acc159f87e41e5f4321d15fc2f70
DaengDo <ringyoon567@gmail.com>
2023-10-21 19:09:54 +0900

    카드 컨테이너와 카드 아이템 요소의 레이아웃 스타일 적용

--- src/components/card/CardItem.tsx
+++ src/components/card/CardItem.tsx
@@ -1,12 +1,22 @@
+import styled from '@emotion/styled';
+
 import { Card as CardType } from '../../types/card';
+
+const StyledCardItem = styled.div`
+  box-sizing: border-box;
+  flex-basis: calc(25% - 40px); // 20px은 부모의 gap
+  padding: 20px;
+  border-radius: 15px;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+`;
 
 export const CardItem = ({ data }: { data: CardType }) => {
   const { audio, category, img, name } = data;
 
   return (
-    <div>
+    <StyledCardItem>
       <img src={img} alt={name} draggable="false" />
       <div>{name}</div>
-    </div>
+    </StyledCardItem>
   );
 };
--- src/pages/Card.tsx
+++ src/pages/Card.tsx
@@ -1,13 +1,22 @@
+import styled from '@emotion/styled';
+
 import { CardItem } from '../components/card/CardItem';
 
 import CARD_MOCK from '../components/card/mock.json';
 
+const StyledCardContainer = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 40px;
+`;
+
 export const Card = () => {
   return (
-    <div>
+    <StyledCardContainer>
       {CARD_MOCK.map((card) => (
         <CardItem data={card} />
       ))}
-    </div>
+    </StyledCardContainer>
   );
 };
