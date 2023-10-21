d6e81aaa25e17853be6dffe7da887666a90cef79
DaengDo <ringyoon567@gmail.com>
2023-10-21 19:14:27 +0900

    카드 이름 요소 스타일 적용

--- src/components/card/CardItem.tsx
+++ src/components/card/CardItem.tsx
@@ -3,11 +3,21 @@ import styled from '@emotion/styled';
 import { Card as CardType } from '../../types/card';
 
 const StyledCardItem = styled.div`
+  display: flex;
+  flex-direction: column;
+
   box-sizing: border-box;
   flex-basis: calc(25% - 40px); // 20px은 부모의 gap
+
   padding: 20px;
   border-radius: 15px;
   box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+  align-items: center;
+`;
+
+const StyledCardName = styled.div`
+  font-weight: bold;
+  font-size: 30px;
 `;
 
 export const CardItem = ({ data }: { data: CardType }) => {
@@ -16,7 +26,7 @@ export const CardItem = ({ data }: { data: CardType }) => {
   return (
     <StyledCardItem>
       <img src={img} alt={name} draggable="false" />
-      <div>{name}</div>
+      <StyledCardName>{name}</StyledCardName>
     </StyledCardItem>
   );
 };
