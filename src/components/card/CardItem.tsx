c578546788bd09527cdb2923e2bcdece45592ee5
DaengDo <ringyoon567@gmail.com>
2023-10-21 15:52:15 +0900

    CardItem 마크업

--- src/components/card/CardItem.tsx
+++ src/components/card/CardItem.tsx
@@ -3,5 +3,10 @@ import { Card as CardType } from '../../types/card';
 export const CardItem = ({ data }: { data: CardType }) => {
   const { audio, category, img, name } = data;
 
-  return <div>{name}</div>;
+  return (
+    <div>
+      <img src={img} alt={name} draggable="false" />
+      <div>{name}</div>
+    </div>
+  );
 };
