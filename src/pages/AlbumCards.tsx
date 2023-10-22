934529e477f61e8644cdabfa4831c9f5df51c641
UHquokka <bananamayong@gmail.com>
2023-10-22 19:26:33 +0900

    fix: albuncard 불러오는 방식 수정

--- src/pages/AlbumCards.tsx
+++ src/pages/AlbumCards.tsx
@@ -1,24 +1,42 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 import CARD_MOCK from '../assets/card/card_mock.json'
 import { useParams } from 'react-router';
+import { CardItem } from '../components/card/CardItem';
+import { CardPage } from '../components/card/CardPage';
 export default function AlbumCards() {
     const { category } = useParams() as { category: string };
-    const [url, setUrl] = useState("");
-    useEffect(() => {
-        setUrl(category);
-    }, [])
+    const [currentPage, setCurrentPage] = useState(0); // 계산 편의성을 위해 첫 페이지를 0으로 설정
+
+    const nextPage = currentPage + 1;
+    const prevPage = currentPage - 1;
+
+    const cardList = CARD_MOCK.filter((c) => c.category === category);
+
+    const hasNextPage = cardList.slice(nextPage * 8, (nextPage + 1) * 8).length !== 0;
+    const hasPrevPage =
+        currentPage !== 0 || cardList.slice(prevPage * 8, currentPage * 8).length !== 0;
+
+    const handleClickNext = () => setCurrentPage((page) => page + 1);
+    const handleClickPrev = () => setCurrentPage((page) => page - 1);
+
+    const startIndex = currentPage * 8;
+    const endIndex = nextPage * 8;
+
 
     return (
         <CardsWrapper>
+            {hasNextPage && <CardPage.Next onClick={handleClickNext} />}
+            {hasPrevPage && <CardPage.Prev onClick={handleClickPrev} />}
             <CardGrid>
-                {CARD_MOCK.filter(card => card.category === url).slice(0, 8).
-                    map(card =>
-                        <div key={card.name}>
-                            <img style={{ width: '90%' }} src={`/${card.img}`} alt={card.name} draggable="false" />
-                        </div>)}
+
+                {CARD_MOCK.filter((c) => c.category === category)
+                    .slice(startIndex, endIndex)
+                    .map((card) => (
+                        <CardItem data={card} key={card.name} />
+                    ))}
             </CardGrid>
-        </CardsWrapper>
+        </CardsWrapper >
     );
 }
 
