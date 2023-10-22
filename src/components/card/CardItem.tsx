6dbcbc957bccd5b6caef4e1c2f215465f7bf4c28
DaengDo <ringyoon567@gmail.com>
2023-10-22 15:37:57 +0900

    카드 모달을 카드 아이템에서 랜더링

--- src/components/card/CardItem.tsx
+++ src/components/card/CardItem.tsx
@@ -1,13 +1,17 @@
-import React, { MouseEventHandler } from 'react';
+import React from 'react';
 import styled from '@emotion/styled';
+import { keyframes } from '@emotion/react';
+import {
+  Dialog,
+  DialogContent,
+  DialogOverlay,
+  DialogPortal,
+  DialogTrigger,
+} from '@radix-ui/react-dialog';
+
+import { useRootRef } from '../../pages/Root';
 
 import { Card as CardType } from '../../types/card';
-
-interface CardItem {
-  data: CardType;
-  setSelectData: (val: CardType) => void;
-  setShowModal: (val: boolean) => void;
-}
 
 const StyledCardItem = styled.div`
   display: flex;
@@ -23,7 +27,6 @@ const StyledCardItem = styled.div`
   border-radius: 15px;
   box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
   align-items: center;
-  cursor: pointer;
 `;
 
 const StyledCardImg = styled.img`
@@ -34,23 +37,65 @@ const StyledCardImg = styled.img`
   background-color: gray;
 `;
 
-const StyledCardName = styled.div`
-  font-weight: bold;
-  font-size: 30px;
+// TODO: Dialog 컴포넌트 추출
+
+const StyledDialogTrigger = styled(DialogTrigger)`
+  background-color: transparent;
+  padding: 0;
+  border: 0;
+  cursor: pointer;
 `;
 
-export const CardItem = ({ data, setShowModal, setSelectData }: CardItem) => {
-  const { audio, category, img, name } = data;
+const overlayShow = keyframes`
+  from { opacity: 0 }
+  to { opacity: 1 }
+`;
 
-  const handleClick = () => {
-    setShowModal(true);
-    setSelectData(data);
-  };
+const StyledDialogOverlay = styled(DialogOverlay)`
+  background-color: rgba(0, 0, 0, 0.2);
+  position: fixed;
+  inset: 0;
+
+  animation-name: ${overlayShow};
+  animation-duration: 150ms;
+  animation-timing-function: cubic-bezier(0.16, 1, 0.3, 1);
+`;
+
+const contentShow = keyframes`
+  from { opacity: 0; transform: translate(-50%, -48%) scale(0.96) }
+  to { opacity: 1; transform: translate(-50%, -50%) scale(1) }
+`;
+
+const StyledDialogContent = styled(DialogContent)`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+
+  transform: translate(-50%, -50%);
+  animation-name: ${contentShow};
+  animation-duration: 150ms;
+`;
+
+export const CardItem = ({ data }: { data: CardType }) => {
+  const { audio, category, img, name } = data;
+  const rootRef = useRootRef();
 
   return (
-    <StyledCardItem onClick={handleClick}>
-      <StyledCardImg src={img} alt={name} draggable="false" />
-      <StyledCardName>{name}</StyledCardName>
-    </StyledCardItem>
+    <Dialog>
+      <StyledDialogTrigger>
+        <StyledCardItem>
+          <StyledCardImg src={img} alt={name} draggable="false" />
+        </StyledCardItem>
+      </StyledDialogTrigger>
+
+      <DialogPortal container={rootRef}>
+        <StyledDialogOverlay />
+        <StyledDialogContent>
+          <StyledCardItem>
+            <StyledCardImg src={img} alt={name} draggable="false" />
+          </StyledCardItem>
+        </StyledDialogContent>
+      </DialogPortal>
+    </Dialog>
   );
 };
