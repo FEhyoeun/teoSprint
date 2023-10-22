3ffd7365c0b316c08c69a622266109ff20abb333
DaengDo <ringyoon567@gmail.com>
2023-10-22 21:08:26 +0900

    fix: 카드 목록 내 페이지 변경 버튼의 위치가 반전된 오류

--- src/components/card/CardPage.tsx
+++ src/components/card/CardPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from '@emotion/styled';
 import Icon from '../common/Icon';
 
@@ -6,17 +5,17 @@ const IconStyled = styled(Icon)<{ isNext: boolean }>`
   position: absolute;
   top: 50%;
   transform: translateY(-50%);
-  left: ${({ isNext }) => isNext && 0};
-  right: ${({ isNext }) => !isNext && 0};
+  right: ${({ isNext }) => isNext && 0};
+  left: ${({ isNext }) => !isNext && 0};
   cursor: pointer;
 `;
 
 const Next = ({ onClick }: { onClick: () => void }) => {
-  return <IconStyled name="chevron-left" isNext size={96} color="white" onClick={onClick} />;
+  return <IconStyled name="chevron-right" isNext size={96} color="white" onClick={onClick} />;
 };
 const Prev = ({ onClick }: { onClick: () => void }) => {
   return (
-    <IconStyled name="chevron-right" isNext={false} size={96} color="white" onClick={onClick} />
+    <IconStyled name="chevron-left" isNext={false} size={96} color="white" onClick={onClick} />
   );
 };
 
