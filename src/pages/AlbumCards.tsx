3b23de8e78d9f7f42adc0ad4501c912a9518e311
UHquokka <bananamayong@gmail.com>
2023-10-22 21:24:32 +0900

    style: album 카드 정렬

--- src/pages/AlbumCards.tsx
+++ src/pages/AlbumCards.tsx
@@ -5,47 +5,45 @@ import { useParams } from 'react-router';
 import { CardItem } from '../components/card/CardItem';
 import { CardPage } from '../components/card/CardPage';
 export default function AlbumCards() {
-  const { category } = useParams() as { category: string };
-  const [currentPage, setCurrentPage] = useState(0); // 계산 편의성을 위해 첫 페이지를 0으로 설정
+    const { category } = useParams() as { category: string };
+    const [currentPage, setCurrentPage] = useState(0); // 계산 편의성을 위해 첫 페이지를 0으로 설정
 
-  const nextPage = currentPage + 1;
-  const prevPage = currentPage - 1;
+    const nextPage = currentPage + 1;
+    const prevPage = currentPage - 1;
 
-  const cardList = CARD_MOCK.filter((c) => c.category === category);
+    const cardList = CARD_MOCK.filter((c) => c.category === category);
 
-  const hasNextPage = cardList.slice(nextPage * 8, (nextPage + 1) * 8).length !== 0;
-  const hasPrevPage =
-    currentPage !== 0 || cardList.slice(prevPage * 8, currentPage * 8).length !== 0;
+    const hasNextPage = cardList.slice(nextPage * 8, (nextPage + 1) * 8).length !== 0;
+    const hasPrevPage =
+        currentPage !== 0 || cardList.slice(prevPage * 8, currentPage * 8).length !== 0;
 
-  const handleClickNext = () => setCurrentPage((page) => page + 1);
-  const handleClickPrev = () => setCurrentPage((page) => page - 1);
+    const handleClickNext = () => setCurrentPage((page) => page + 1);
+    const handleClickPrev = () => setCurrentPage((page) => page - 1);
 
-  const startIndex = currentPage * 8;
-  const endIndex = nextPage * 8;
+    const startIndex = currentPage * 8;
+    const endIndex = nextPage * 8;
 
-  return (
-    <CardsWrapper>
-      {hasNextPage && <CardPage.Next onClick={handleClickNext} />}
-      {hasPrevPage && <CardPage.Prev onClick={handleClickPrev} />}
-      <CardGrid>
-        {CARD_MOCK.filter((c) => c.category === category)
-          .slice(startIndex, endIndex)
-          .map((card) => (
-            <CardItem data={card} key={card.name} />
-          ))}
-      </CardGrid>
-    </CardsWrapper>
-  );
+    return (
+        <CardsWrapper>
+            {hasNextPage && <CardPage.Next onClick={handleClickNext} />}
+            {hasPrevPage && <CardPage.Prev onClick={handleClickPrev} />}
+            {CARD_MOCK.filter((c) => c.category === category)
+                .slice(startIndex, endIndex)
+                .map((card) => (
+                    <CardItem data={card} key={card.name} />
+                ))}
+        </CardsWrapper>
+    );
 }
 
 const CardsWrapper = styled.div`
-  display: flex;
-`;
-
-const CardGrid = styled.div`
+    position: relative;
   width: 100%;
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-template-rows: auto;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
   overflow: hidden;
 `;
