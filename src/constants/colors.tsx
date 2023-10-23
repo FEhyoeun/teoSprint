a254ff022b4ebe17d2f72598427e26fb5f0094f5
FEhyoeun <khyoeun92@gmail.com>
2023-10-23 14:16:18 +0900

    feat: face 이미지 변경 및 배경색 추가, Profile에서 이름 주석 처리

--- src/constants/colors.tsx
+++ src/constants/colors.tsx
@@ -3,3 +3,4 @@ export const _mainCol200 = '#e77f67';
 export const _mainCol300 = '#778beb';
 export const _mainCol400 = '#f7d794';
 export const _mainTextDark = 'black';
+export const _mainColBlue = '#A0E1F5';
