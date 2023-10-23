548f0f6d9f27ecb3068e1b437e98478f8c8d15c7
UHquokka <bananamayong@gmail.com>
2023-10-23 14:45:05 +0900

    design: 컬러명 변경 및 컬러 추가

--- src/constants/colors.tsx
+++ src/constants/colors.tsx
@@ -1,7 +1,9 @@
 export const _mainColOrange = '#e77f67';
+export const _mainColOrange_Dark = '#E38846';
 export const _mainColGreen = '#A0D36D';
 export const _mainColBlue = '#49D1CC';
-export const _mainColDarkGreen = '#69B95C';
+export const _mainColGreen_Dark = '#69B95C';
 
+export const _mainColBg_Light = '#FBF9F7'
 export const _mainColBg_Dark = '#EDEAE3'
 export const _mainTextDark = 'black';
